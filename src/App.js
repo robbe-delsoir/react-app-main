3dc0b268c3dc04407fd600992f60b196cffdb829
robbe.delsoir <robbe.delsoir@student.vives.be>
2021-12-09 12:04:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ApiInfo from './components/ApiInfo'
-
 
 function App() {
-  return (
-
-  <div>
-  <body>
-    <div className="header">
-      <Header/>
-    </div>
-    <div class="api">
-    <ApiInfo/>
-    </div>
-    <div className="footer">
-      <Footer/>
-    </div>
-    </body>
-  </div>
-  );
-
-  
+  const value = 'World';
+  return <div>Hello {value}</div>;
 }
 
-
 export default App;
-
