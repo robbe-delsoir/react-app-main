3917d563442b8ab099c6dcbf43ac21ceb3b540f2
robbe.delsoir <robbe.delsoir@student.vives.be>
2021-12-09 12:08:41 +0100

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <p>Mickiesoft help clients to build better mobile and web products faster for an affordable price.We are sensitive to to your brand and business goals.We build smart, robust and scalable Web and Mobile Solutions for StartUps to Unicorns.</p>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <h1>MickieSoft Movies</h1>
+    )
+}
+
+export default Header;
